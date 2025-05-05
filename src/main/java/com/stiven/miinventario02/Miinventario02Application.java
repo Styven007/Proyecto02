9c4963417a3c476c7a2f6d352689a85bdec19aee
Styven007 <reyesstiven873@gmail.com>
2025-05-04 23:27:00 -0500

    Guardando segundo commit para spring boot

---
+++ src/main/java/com/stiven/miinventario02/Miinventario02Application.java
@@ -0,0 +1,13 @@
+package com.stiven.miinventario02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miinventario02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miinventario02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/stiven/miinventario02/Miinventario02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stiven.miinventario02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miinventario02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
